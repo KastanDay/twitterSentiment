eb3e31e965561da9109d0d870834f6bc48fd480f
kastan <kastanvday@gmail.com>
2017-02-04 16:29:10 -0500

    Initial file import

---
+++ twitterSentiment.py
@@ -0,0 +1,23 @@
+import tweepy
+from textblob import TextBlob
+
+consumer_key = '9QCcbB1H06623O0jcZs0fFh3W'
+consumer_secret = 'VKRX3iLmewqr07fz066J7gisYQoKMXPcMtV45pXqwebcB2FndT'
+
+access_token = '1262219683-BRhwVmBtPvPP5ssXV9yFsvLoIvpjx8BsXOsZpUE'
+access_token_secret = 'DmtbwZnWXGWmvp1VSPTvthctR4SSun25nx2tifxZcjz7U'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print("-------")
+	print(analysis.sentiment)
+	print("-------")
+	print("\n")
